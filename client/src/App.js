66a5e6d22ec7afd2eac8683c7de733cde7db260a
MKHLink <likhonhasankomol@gmail.com>
2023-03-07 15:54:42 -0500

    login page working as intended, mutation working

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,33 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import SignUp from "./Pages/SignUp";
 import LandingPage from "./Pages/Home";
 
+import {setContext} from '@apollo/client/link/context';
+import {ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+const httpLink = createHttpLink({
+  uri:'http://localhost:3001/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
-   <main>
+   <ApolloProvider client={client}>
     <Router>
       <Routes>
         <Route path='/' element={<Login/>}/>
@@ -14,7 +38,7 @@ function App() {
         <Route path='/landingpage' element={<LandingPage/>}/>
       </Routes>
     </Router>
-   </main>
+   </ApolloProvider>
   );
 }
 
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {useMutation} from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
+import Auth from '../utils/auth';
 
-  import { Col, Row } from 'antd';
-  import { Button, Checkbox, Form, Input } from 'antd';
-
+import { Col, Row } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
   
-
 const Login = ()=>{
 
-   
+   const [formState, setFormState] = useState({email: '', password: ''});
+   const [userLogin, {error}] = useMutation(LOGIN_USER);
 
+   const handleChange = (event)=>{
+    const {name, value} = event.target;
+  
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  
+  const handleFormSubmit = async event =>{
+    event.preventDefault();
+    
+    try{
+      const {data} = await userLogin(
+        {
+          variables: {...formState}
+        }
+        
+      );
+      console.log(data);
+      Auth.login(data.userLogin.token);
+    }catch(e){
+      console.log(e);
+    }
+  };
+    
     return(
         <div style={{marginTop:50}}>
             <Row>
             <Col xs={20} sm={16} md={12} lg={8} xl={8}>
-            <Form
+            {/* <Form 
     name="basic"
     labelCol={{
       span: 8,
@@ -29,11 +57,12 @@ const Login = ()=>{
       remember: true,
     }}
     
+    
     autoComplete="off"
   >
     <Form.Item
       label="Username"
-      name="username"
+      
       rules={[
         {
           required: true,
@@ -41,12 +70,12 @@ const Login = ()=>{
         },
       ]}
     >
-      <Input />
+      <Input name="email" type="email" value={formState.email} onChange={handleChange}/>
     </Form.Item>
 
     <Form.Item
       label="Password"
-      name="password"
+      
       rules={[
         {
           required: true,
@@ -54,7 +83,7 @@ const Login = ()=>{
         },
       ]}
     >
-      <Input.Password />
+      <Input.Password name="password" type="password" value={formState.password} onChange={handleChange}/>
     </Form.Item>
 
     <Form.Item
@@ -74,14 +103,37 @@ const Login = ()=>{
         span: 16,
       }}
     >
-      <Button type="primary" htmlType="submit">
+      <Button type="primary" htmlType="submit" onSubmit={handleFormSubmit}>
         Submit
       </Button>
       <p>Don't have an account? Register <a href='/signup'>here</a></p>
     </Form.Item>
-  </Form>
+  </Form> */}
 
-            
+<form onSubmit={handleFormSubmit}>
+              <input
+                className='form-input'
+                placeholder='Your email'
+                name='email'
+                type='email'
+                id='email'
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className='btn d-block w-100' type='submit'>
+                Submit
+              </button>
+            </form>
+  {error && <div>Login Failed</div>}
             </Col>
             </Row>
         </div>
